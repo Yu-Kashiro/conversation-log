727d33a3f4be77c9d8324d4ca4e64c45015cdbae
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-10-13 12:52:28 +0900

    feat: 会話管理機能を追加し、conversationFormData型を定義

---
+++ actions/conversation.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { db } from "@/db";
+import { conversations } from "@/db/schemas/conversation";
+import { verifySession } from "@/lib/session";
+import { ConversationFormData } from "@/types/conversation";
+import { conversationFormSchema } from "@/zod/conversation";
+import { and, eq } from "drizzle-orm";
+
+export async function createConversation(formData: ConversationFormData) {
+  const data = conversationFormSchema.parse(formData);
+  const session = await verifySession();
+  const caseworkerId = session.user.id;
+
+  await db.insert(conversations).values({ ...data, caseworkerId });
+}
+
+export async function updateConversation(
+  id: string,
+  formData: ConversationFormData
+) {
+  const data = conversationFormSchema.parse(formData);
+  const session = await verifySession();
+  const caseworkerId = session.user.id;
+
+  await db
+    .update(conversations)
+    .set({ ...data, caseworkerId })
+    .where(
+      and(
+        eq(conversations.id, id),
+        eq(conversations.caseworkerId, caseworkerId)
+      )
+    );
+}
+
+export async function deleteConversation(id: string) {
+  const session = await verifySession();
+  const caseworkerId = session.user.id;
+
+  await db
+    .delete(conversations)
+    .where(
+      and(
+        eq(conversations.id, id),
+        eq(conversations.caseworkerId, caseworkerId)
+      )
+    );
+}
--- types/conversation.ts
+++ types/conversation.ts
@@ -1,3 +1,6 @@
 import { conversations } from "@/db/schemas/conversation";
+import { conversationFormSchema } from "@/zod/conversation";
+import z from "zod";
 
 export type Conversation = typeof conversations.$inferSelect;
+export type ConversationFormData = z.infer<typeof conversationFormSchema>;
